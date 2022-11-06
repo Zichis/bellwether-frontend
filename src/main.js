69841e12daecce85f976b0afbe7100878de47325
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-06 22:28:16 +0100

    Installation and setup of tailwindcss.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "./css/main.css";
 import "./config/axios";
 
 Vue.config.productionTip = false;
