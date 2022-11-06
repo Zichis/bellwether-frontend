7a601b631a801e25135ddf29e07f2c4e04429d77
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-06 22:01:09 +0100

    Setup auth page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,18 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/auth/",
+    name: "Auth",
+    component: () => import("../views/Auth.vue"),
+    children: [
+      {
+        path: "login",
+        name: "Login",
+        component: Login,
+      },
+    ],
   },
 ];
 
