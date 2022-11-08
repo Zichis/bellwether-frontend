84dc36a2eb88a8d4da7a638126591f0763eacf5c
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-08 04:23:40 +0100

    Added login feature.

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,7 +1,50 @@
 <template>
   <div>
-    <h1>Auth</h1>
-    <router-view></router-view>
+    <div class="flex min-h-screen">
+      <div
+        class="hidden md:p-5 lg:p-10 md:block md:w-1/3"
+        style="background-image: linear-gradient(rgba(31,50,77,0.9),rgba(31,50,77,1)), url('https://images.unsplash.com/photo-1552664688-cf412ec27db2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80')"
+      >
+        <h2 class="font-extrabold text-2xl text-yellow-50">BELLWETHER</h2>
+        <div class="flex justify-center items-center h-full">
+          <div>
+            <h3 class="text-4xl text-white font-light mb-5">
+              Get started managing your tasks with our new features
+            </h3>
+            <ul class="pl-5">
+              <li class="text-2xl mb-4 animate-1">
+                <font-awesome-icon
+                  icon="check-circle"
+                  class="text-green-400 mr-2"
+                ></font-awesome-icon>
+                <span class="font-extralight text-white">
+                  Track Personal Tasks
+                </span>
+              </li>
+              <li class="text-2xl mb-4 animate-2">
+                <font-awesome-icon
+                  icon="check-circle"
+                  class="text-green-400 mr-2"
+                ></font-awesome-icon>
+                <span class="font-extralight text-white">
+                  Create Projects
+                </span>
+              </li>
+              <li class="text-2xl mb-4 animate-3">
+                <font-awesome-icon
+                  icon="check-circle"
+                  class="text-green-400 mr-2"
+                ></font-awesome-icon>
+                <span class="font-extralight text-white">
+                  Invite friends for collaboration
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,87 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <router-view></router-view>
+  <!--<div>
+    <h1 class="text-red-500">Login</h1>
+  </div>-->
+  <div
+    class="flex-1 bg-gray-50 flex justify-center items-center px-5 md:px-10 h-screen"
+  >
+    <div class="w-full">
+      <header class="px-5 text-gray-700 mb-8">
+        <h2 class="text-2xl font-semibold mb-10">Login</h2>
+        <h3 class="font-semibold mb-2">Log into your account</h3>
+        <p class="font-light">
+          Welcome back to Bellwether. Enter your account details to continue
+          from where you stopped.
+        </p>
+      </header>
+      <form @submit.prevent="login" class="w-full">
+        <div class="flex flex-wrap">
+          <div class="w-full lg:w-1/2 px-5 mb-5">
+            <label for="email" class="block mb-2 text-gray-500">Email</label>
+            <input
+              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              type="text"
+              name="email"
+              v-model="loginForm.email"
+              placeholder="Enter email"
+            />
+          </div>
+          <div class="w-full lg:w-1/2 px-5 mb-5">
+            <label for="password" class="block mb-2 text-gray-500"
+              >Password</label
+            >
+            <input
+              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              type="password"
+              name="password"
+              v-model="loginForm.password"
+              placeholder="Enter password"
+            />
+          </div>
+          <button
+            class="bg-blue-400 font-light px-3 py-1 mb-2 mx-5 rounded text-blue-100 hover:text-white hover:bg-blue-500"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+        <div v-if="loginErrorMessage !== null">
+          <p class="my-1 px-5 text-red-400 font-light">
+            {{ loginErrorMessage }}
+          </p>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import AuthService from "../services/AuthService";
+import router from "../router";
+export default {
+  data() {
+    return {
+      loginForm: {
+        email: "",
+        password: "",
+      },
+      loginErrorMessage: null,
+    };
+  },
+  methods: {
+    login() {
+      AuthService.login(this.loginForm)
+        .then((response) => {
+          localStorage.setItem("bellwether_token", response.data.token);
+          //this.$store.dispatch("user", response.data.user);
+          router.push("/");
+        })
+        .catch(
+          (error) => (this.loginErrorMessage = error.response.data.message)
+        );
+    },
+  },
+};
 </script>
 
 <style></style>
