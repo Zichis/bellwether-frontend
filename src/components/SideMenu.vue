7bcdd5ab9bd6bc4cb4b3113b9fb33101e625a558
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-15 14:14:24 +0100

    Created sidemenu.

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="font-bold text-center">Sidemenu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style></style>
