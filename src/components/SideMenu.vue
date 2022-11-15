dea22c5906cb538daacbcda2de7081c608395f96
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-15 14:42:50 +0100

    Added logo.

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h1 class="font-bold text-center">Sidemenu</h1>
+  <div class="lg:w-64" style="background: #05082c">
+    <div class="p-5">
+      <img
+        class="hidden lg:block"
+        src="../assets/bellwether.jpeg"
+        alt="bellwether_logo"
+      />
+    </div>
+    <div class="p-5">
+      <router-link to="/app/dashboard" class="text-gray-200">
+        Dashboard
+      </router-link>
+    </div>
   </div>
 </template>
 
