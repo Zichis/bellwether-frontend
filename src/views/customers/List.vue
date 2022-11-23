5343cef7d011933b02a8b1f6acd51620cd0457a0
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-23 06:49:33 +0100

    Updated code.

--- src/views/customers/List.vue
+++ src/views/customers/List.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
     <h1 class="mb-10 font-semibold text-3xl">Customers</h1>
+    <Table :tableData="customers" :titles="titles" />
   </div>
 </template>
 
 <script>
+import CustomerService from "../../services/CustomerService";
+import Table from "../../components/Table.vue";
 export default {
+  components: { Table },
   data() {
     return {
-      customers: [{ name: "Ezichi Ebere", email: "tripleee@yahoo.co.uk" }],
+      customers: [],
+      titles: ["Name", "Email", "Address", "Phone"],
+      errorMessage: "",
     };
+  },
+  created() {
+    CustomerService.index()
+      .then((response) => {
+        const customers = response["data"]["data"];
+        this.customers = customers;
+      })
+      .catch((error) => (this.errorMessage = error.response.data.message));
   },
 };
 </script>
