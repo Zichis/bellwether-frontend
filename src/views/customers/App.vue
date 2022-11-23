330ae581fda31b04fd73d90d3a13bc16aeb93d8d
Ezichi Ebere <tripleee@yahoo.co.uk>
2022-11-23 06:35:10 +0100

    Added group links

--- src/views/customers/App.vue
+++ src/views/customers/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="p-5 bg-gray-100">
+    <div class="inline-flex mb-5 shadow-md rounded-md overflow-hidden">
+      <router-link
+        to="/app/customers"
+        class="bg-gray-700 hover:bg-gray-600 text-gray-200 block px-2 py-1 border border-gray-600"
+      >
+        <font-awesome-icon icon="eye" class="lg:mr-2" />
+        <span class="hidden lg:inline font-thin text-sm">VIEW ALL</span>
+      </router-link>
+      <router-link
+        to="/app/customers/add"
+        class="bg-gray-700 hover:bg-gray-600 text-gray-200 block px-2 py-1 border border-gray-600"
+      >
+        <font-awesome-icon icon="user-plus" class="lg:mr-2" />
+        <span class="hidden lg:inline font-thin text-sm">ADD NEW</span>
+      </router-link>
+    </div>
     <router-view />
   </div>
 </template>
@@ -8,4 +24,4 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped></style>
