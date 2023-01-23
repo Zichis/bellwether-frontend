f354dcc983640d7e526efe034278447fa65e414e
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-01-23 10:39:07 +0100

    Added new pages for website.

---
+++ src/views/website/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center items-center pt-20 bg-gray-100 text-blue-900 h-screen overflow-y-scroll">
+    About page coming soon...
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  components: {},
+};
+</script>
---
+++ src/views/website/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>
+        <NavBar />
+        <router-view />
+    </div>
+</template>
+<script lang="js">
+import NavBar from "../../components/NavBar.vue";
+export default {
+    components: { NavBar },
+}
+</script>
+<style lang="">
+    
+</style>
