1a3fe88ecf4749f5aa8acc3f3ba1ea1abacab2f5
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-01-28 19:33:52 +0100

    Added new view.

---
+++ src/views/website/GetStarted.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="flex justify-center items-center pt-20 bg-gray-100 text-blue-900 h-screen overflow-y-scroll">
+      <form @submit.prevent="apply" class="w-full lg:w-1/3">
+        <div class="shadow rounded bg-white m-5 p-5">
+          <form @submit.prevent="apply" class="w-full"></form>
+          <h1 class="font-bold text-2xl">Get Started</h1>
+          <hr class="my-2" />
+          <p class="mb-5">Fill the form to join the Bellwether family</p>
+          <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+            <input type="text" name="name" v-model="getStartedForm.name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" name="email" v-model="getStartedForm.email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone</label>
+            <input type="text" name="phone" v-model="getStartedForm.phone_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address 1</label>
+            <input type="text" name="address_1" v-model="getStartedForm.address_1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address 2</label>
+            <input type="text" name="address_2" v-model="getStartedForm.address_2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          </div>
+          <div class="flex justify-between">
+            <div class="mb-6">
+              <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">State</label>
+              <input type="text" name="state" v-model="getStartedForm.state" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+            <div class="mb-6">
+              <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Local Government</label>
+              <input type="text" name="local_government" v-model="getStartedForm.local_government" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </div>
+          </div>
+          <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SUBMIT</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "GetStartedPage",
+    components: {},
+    data() {
+      return {
+        getStartedForm: {
+          name: "",
+          email: "",
+          phone_number: "",
+          address_1: "",
+          address_2: "",
+          state: "",
+          local_government: "",
+        },
+        errorMessage: null,
+      };
+    },
+    methods: {
+      apply() {
+        console.log("Form sent!!!");
+      }
+    }
+  };
+  </script>
