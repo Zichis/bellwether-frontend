14d29a48be7eeb9eb027949058d835c328c066b9
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-01 15:25:05 +0100

    Changed wording

--- src/views/website/Home.vue
+++ src/views/website/Home.vue
@@ -7,7 +7,7 @@
           YOU HAVE WASTE RIGHT?
         </h1>
         <h1 class="animate__animated animate__slideInUp font-bold text-4xl text-yellow-400">WE REMOVE</h1>
-        <router-link to="/get-started" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-10 inline-block">Get started</router-link>
+        <router-link to="/get-started" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 my-10 inline-block">Register Here</router-link>
       </div>
     </div>
     <div class="text-center p-10 lg:p-20 bg-gray-800 text-white">
