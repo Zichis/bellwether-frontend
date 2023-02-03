1774bff51877c67c10d9e13e59fcd8a3640339b1
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-03 08:03:39 +0100

    Changed phone_number to phone.

--- src/views/website/GetStarted.vue
+++ src/views/website/GetStarted.vue
@@ -24,7 +24,7 @@
             <div class="w-full">
               <div class="m-2">
                 <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone <span class="text-red-500">*</span></label>
-                <input type="text" name="phone" v-model="getStartedForm.phone_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Eg. 08000000000" required>
+                <input type="text" name="phone" v-model="getStartedForm.phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Eg. 08000000000" required>
               </div>
             </div>
           </div>
@@ -199,7 +199,7 @@
         getStartedForm: {
           name: "",
           email: "",
-          phone_number: "",
+          phone: "",
           address_1: "",
           address_2: "",
           state: "",
