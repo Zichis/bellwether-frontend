8efccfb2d397094c249108ab81839abe525dc855
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-07 18:36:00 +0100

    Added new views.

---
+++ src/views/pending-customers/List.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+      <h1 class="mb-10 font-semibold text-3xl">Pending Approvals</h1>
+      <CustomerTable
+        :tableData="customers"
+        :titles="titles"
+        @delete="deleteCustomer"
+        @edit="editCustomer"
+        @view="viewCustomer"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import CustomerService from "../../services/PendingCustomerService";
+  import CustomerTable from "../../components/PendingCustomersTable.vue";
+  import DisplayAlert from "../../functions/DisplayAlert";
+  import SetAlert from "../../functions/SetAlert";
+  import router from "../../router";
+  export default {
+    name: "ListPendingCustomers",
+    components: { CustomerTable },
+    data() {
+      return {
+        customers: [],
+        titles: ["Name", "Email", "Address", "Phone"],
+        errorMessage: "",
+      };
+    },
+    created() {
+      CustomerService.index()
+        .then((response) => {
+          console.log(response);
+          const customers = response["data"];
+          this.customers = customers;
+          console.log(this.customers);
+        })
+        .catch((error) => (this.errorMessage = error.response.data.message));
+    },
+    mounted() {
+      DisplayAlert(this.$swal);
+    },
+    methods: {
+      deleteCustomer(id) {
+        CustomerService.delete(id)
+          .then((response) => {
+            const customers = response["data"]["data"];
+            this.customers = customers;
+            SetAlert("Deleted", "You have deleted customer!", "success");
+            DisplayAlert(this.$swal);
+          })
+          .catch((error) => (this.errorMessage = error.response.data.message));
+      },
+      editCustomer(id) {
+        router.push(`/app/customers/${id}/edit`);
+      },
+      viewCustomer(id) {
+        router.push(`/app/pending-approvals/${id}`);
+      },
+    },
+  };
+  </script>
+  
+  <style></style>
+  
---
+++ src/views/pending-customers/Show.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="">
+        <div class="flex flex-col items-center p-5 md:p-20 rounded shadow-md mb-10" style="background: url(https://images.pexels.com/photos/322461/pexels-photo-322461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2); background-repeat: no-repeat; background-size: cover;">
+            <!--<img :src="customer?.avatar" alt="avatar" class="rounded-full w-32 border-2">-->
+            <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            class="rounded-full w-20 border-2 border-green-300"
+            alt="Avatar"
+            />
+            <p class="my-2 text-white text-2xl md:text-4xl font-bold">{{ customer?.name }}</p>
+        </div>
+        <div class="flex flex-wrap">
+            <div class="w-full md:w-1/2 p-3">
+                <div class="bg-white p-2 rounded shadow-md md:h-64 overflow-y-scroll">
+                    <h2 class="font-bold text-blue-500">Bio</h2>
+                    <hr class="my-3" />
+                    <div class="flex flex-wrap">
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Name</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.name }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Email</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.email }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Phone</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.phone }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 p-3">
+                <div class="bg-white p-2 rounded shadow-md md:h-64 overflow-y-scroll">
+                    <h2 class="font-bold text-yellow-500">Address</h2>
+                    <hr class="my-3" />
+                    <div class="flex flex-wrap">
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Address 1</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.address_1 }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Address 2</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.address_2 }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">State</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.state }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Local Government</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.local_government }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Town</p>
+                            <p class="text-gray-800 text-xl">{{ customer?.community }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 p-3">
+                <div class="bg-white p-2 rounded shadow-md md:h-64 overflow-y-scroll">
+                    <h2 class="font-bold text-green-500">Plans</h2>
+                    <hr class="my-3" />
+                    <div class="flex flex-wrap">
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Location Type</p>
+                            <p class="text-gray-800 text-xl capitalize">{{ customer?.location_type }}</p>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Service Plan</p>
+                            <p class="text-gray-800 text-xl capitalize">{{ customer?.service_plan }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 p-3">
+                <div class="bg-white p-2 rounded shadow-md h-64 overflow-y-scroll">
+                    <h2 class="font-bold text-red-500">Others</h2>
+                    <hr class="my-3" />
+                    <div class="flex flex-wrap">
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">ID Photo</p>
+                            <a :href="customer?.id_photo" class="hover:underline" target="_blank" rel="noopener noreferrer">Click to view</a>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Signature</p>
+                            <a :href="customer?.signature" class="hover:underline" target="_blank" rel="noopener noreferrer">Click to view</a>
+                        </div>
+                        <div class="w-full md:w-1/2 mb-2">
+                            <p class="text-gray-600">Referral Code</p>
+                            <p class="text-gray-800 text-xl capitalize">{{ customer?.referral_code }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import CustomerService from "../../services/PendingCustomerService";
+export default {
+    name: "ShowPendingCustomer",
+    data() {
+        return {
+            customer: null
+        };
+    },
+    created() {
+        CustomerService.show(this.$route.params.id)
+            .then((response) => {
+                console.log(response['data']);
+                this.customer = response['data'];
+            })
+            .catch((error) => (this.errorMessage = error.response.data.message));
+    }
+}
+</script>
+
+<style></style>
+  
