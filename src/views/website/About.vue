2a80f0e13418f32fdf8e246e1df6233ece25342d
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-09 20:56:55 +0100

    Updated about page.

--- src/views/website/About.vue
+++ src/views/website/About.vue
@@ -1,6 +1,47 @@
 <template>
-  <div class="flex justify-center items-center pt-20 bg-gray-100 text-blue-900 h-screen overflow-y-scroll">
-    About page coming soon...
+  <div class="pt-20 pb-10 px-10 bg-gray-100 text-blue-900 h-screen overflow-y-scroll">
+    <p class="w-full md:w-3/4">
+      Bellwether Recycling Limited (BR) is a provider of top notch sustainable circular economy waste management and
+      environmental services. She has experience in all phases of solid waste management, which includes waste
+      collection, waste recycling, resource recovery/management, as well as process waste reduction.
+    </p>
+
+    <h3 class="mt-10 mb-2 font-bold">Vision Statement</h3>
+    <p class="w-full md:w-3/4">
+      To Provide waste management services, practices, solutions, and policies that encourage a sustainable circular
+      economy, which Seeks to decrease process waste, over extraction of fresh raw materials for production, eliminate
+      improper waste management practices, techniques, such as Landfill disposal practices, roadside and ocean dumping,
+      mixing of various waste streams etc.
+    </p>
+    <h3 class="mt-10 mb-2 font-bold">Mission Statement</h3>
+    <p class="w-full md:w-3/4">
+      To provide state-of-the-art, cutting-edge waste management services, strategies, solutions and technologies on a
+      consistent basis and at every touch point, that will support a sustainable circular economy
+    </p>
+    <h3 class="mt-10 mb-2 font-bold">Goals Statement</h3>
+    <p class="w-full md:w-3/4">
+      <ul class="pl-5">
+        <li class="list-disc">To promote environmental preservation through appropriate, efficient, and sustainable circular economy waste management techniques, solutions, and policies.</li>
+        <li class="list-disc">To deliver a superior waste collection service in order to safeguard the health and wellness of people, and the ecosystem at large.</li>
+        <li class="list-disc">To promote the reduction of greenhouse gas emissions.</li>
+        <li class="list-disc">To provide sustainable public education about circular economy waste management strategies.</li>
+        <li class="list-disc">To become one of the leading players in the waste management sector.</li>
+        <li class="list-disc">To ensure continual waste recycling until the use of landfills is eliminated.</li>
+      </ul>
+    </p>
+    <h3 class="mt-10 mb-2 font-bold">Company Core Values</h3>
+    <p class="w-full md:w-3/4">
+      <ul class="pl-5">
+        <li class="list-disc">Integrity</li>
+        <li class="list-disc">Excellence</li>
+        <li class="list-disc">Sustainability</li>
+        <li class="list-disc">Continuous Growth</li>
+        <li class="list-disc">Safety</li>
+        <li class="list-disc">Passion</li>
+        <li class="list-disc">Team Work</li>
+        <li class="list-disc">Healthy Relationship</li>
+      </ul>
+    </p>
   </div>
 </template>
 
