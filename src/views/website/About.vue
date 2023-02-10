d9b8225ac0ccd159e48afeb10543c6e9d9acad05
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-10 21:16:20 +0100

    Styled about page.

--- src/views/website/About.vue
+++ src/views/website/About.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="pt-20 pb-10 px-10 bg-gray-100 text-blue-900 h-screen overflow-y-scroll">
-    <p class="w-full md:w-3/4">
-      Bellwether Recycling Limited (BR) is a provider of top notch sustainable circular economy waste management and
-      environmental services. She has experience in all phases of solid waste management, which includes waste
-      collection, waste recycling, resource recovery/management, as well as process waste reduction.
-    </p>
+    <div class="p-5 mb-5 shadow-xl text-white rounded" style="background: #050826;">
+      <p class="w-full md:w-3/4">
+        Bellwether Recycling Limited (BR) is a provider of top notch sustainable circular economy waste management and
+        environmental services. She has experience in all phases of solid waste management, which includes waste
+        collection, waste recycling, resource recovery/management, as well as process waste reduction.
+      </p>
+    </div>
 
-    <h3 class="mt-10 mb-2 font-bold">Vision Statement</h3>
-    <p class="w-full md:w-3/4">
-      To Provide waste management services, practices, solutions, and policies that encourage a sustainable circular
-      economy, which Seeks to decrease process waste, over extraction of fresh raw materials for production, eliminate
-      improper waste management practices, techniques, such as Landfill disposal practices, roadside and ocean dumping,
-      mixing of various waste streams etc.
-    </p>
-    <h3 class="mt-10 mb-2 font-bold">Mission Statement</h3>
-    <p class="w-full md:w-3/4">
-      To provide state-of-the-art, cutting-edge waste management services, strategies, solutions and technologies on a
-      consistent basis and at every touch point, that will support a sustainable circular economy
-    </p>
-    <h3 class="mt-10 mb-2 font-bold">Goals Statement</h3>
+    <div class="p-5 mb-5 shadow-xl text-white rounded" style="background: #d2d2d2; color: #050826;">
+      <h3 class="mb-2 font-bold">Vision Statement</h3>
+      <p class="w-full md:w-3/4">
+        To Provide waste management services, practices, solutions, and policies that encourage a sustainable circular
+        economy, which Seeks to decrease process waste, over extraction of fresh raw materials for production, eliminate
+        improper waste management practices, techniques, such as Landfill disposal practices, roadside and ocean dumping,
+        mixing of various waste streams etc.
+      </p>
+    </div>
+    <div class="p-5 mb-5 shadow-xl text-white rounded" style="background: #b70606; color: white;">
+      <h3 class="mb-2 font-bold">Mission Statement</h3>
+      <p class="w-full md:w-3/4">
+        To provide state-of-the-art, cutting-edge waste management services, strategies, solutions and technologies on a
+        consistent basis and at every touch point, that will support a sustainable circular economy
+      </p>
+    </div>
+    <div class="p-5 mb-5 shadow-xl text-white rounded" style="background: #050826;">
+    <h3 class="mb-2 font-bold">Goals Statement</h3>
     <p class="w-full md:w-3/4">
       <ul class="pl-5">
         <li class="list-disc">To promote environmental preservation through appropriate, efficient, and sustainable circular economy waste management techniques, solutions, and policies.</li>
@@ -29,7 +36,8 @@
         <li class="list-disc">To ensure continual waste recycling until the use of landfills is eliminated.</li>
       </ul>
     </p>
-    <h3 class="mt-10 mb-2 font-bold">Company Core Values</h3>
+    <hr class="my-5">
+    <h3 class="mb-2 font-bold">Company Core Values</h3>
     <p class="w-full md:w-3/4">
       <ul class="pl-5">
         <li class="list-disc">Integrity</li>
@@ -42,6 +50,7 @@
         <li class="list-disc">Healthy Relationship</li>
       </ul>
     </p>
+  </div>
   </div>
 </template>
 
