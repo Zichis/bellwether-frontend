dc9b8c9528edf6cf14c8340a0644e85a5352d2ac
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-11 13:51:20 +0100

    Changed login form style.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,7 +19,7 @@
           <div class="w-full lg:w-1/2 px-5 mb-5">
             <label for="email" class="block mb-2 text-gray-500">Email</label>
             <input
-              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               type="text"
               name="email"
               v-model="loginForm.email"
@@ -31,7 +31,7 @@
               >Password</label
             >
             <input
-              class="px-2 py-1 w-full border-2 border-gray-300 rounded focus:outline-none focus:border-gray-400"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               type="password"
               name="password"
               v-model="loginForm.password"
@@ -39,7 +39,7 @@
             />
           </div>
           <button
-            class="bg-blue-400 font-light px-3 py-1 mb-2 mx-5 rounded text-blue-100 hover:text-white hover:bg-blue-500"
+            class="mx-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             type="submit"
           >
             Login
